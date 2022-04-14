1b639ed5e7fc465bb5b0afa5f3b75dcc99363fc8
Ricky-Prophete <97249245+Ricky-Prophete@users.noreply.github.com>
2022-04-14 19:02:20 -0400

    light modifications for reproducibility

--- Data_wrangling/Energy_Usage_Wrangling.R
+++ Data_wrangling/Energy_Usage_Wrangling.R
@@ -5,20 +5,25 @@ library(lubridate)
 #import dataset
 Energy.Usage <- read.csv('./Data/Raw/Emission_Predictors.csv', stringsAsFactors = FALSE)
 
-#wrangle energy
-Energy.Usage.clean <- Energy.Usage %>% slice(c(4, 32)) %>%
+#isolate values of interest
+Energy.Usage.clean <- Energy.Usage %>% 
+  slice(c(4, 32)) %>%
   select(-c(X:X.1)) #%>%
 row_to_names(row_number = 1) 
 
-Energy.Usage.transposed <- as.data.frame(t(Energy.Usage.clean)) %>%
+#transpose data frame; rename columns
+Energy.Usage.transposed <- 
+  as.data.frame(t(Energy.Usage.clean)) %>%
   row_to_names(row_number = 1) %>%
   rownames_to_column() %>%
   slice(2:57) %>%
   select(-c("rowname"))
 
 #convert values to numeric
+Energy.Usage.transposed$`Total energy consumption`<- 
+  as.numeric(gsub(",", "", Energy.Usage.transposed$`Total energy consumption`))
 
-Energy.Usage.transposed$`Total energy consumption`<- as.numeric(gsub(",", "", Energy.Usage.transposed$`Total energy consumption`))
+Energy.Usage.transposed.clean <- Energy.Usage.transposed.clean
 
 #save
 write.csv(Energy.Usage.transposed, file = './Data/Processed/Energy_Usage_Clean.csv', row.names = FALSE)
